795fc16b5cf296661bdcbddafe07c6c4afc80cc1
xincheng <wxcxincheng@163.com>
2017-05-03 16:02:28 +0800

    populating-next-right-pointers-in-each-node

---
+++ Algorithm/populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,29 @@
+
+//Definition for binary tree with next pointer.
+#include<iostream>
+using namespace std;
+
+struct TreeLinkNode {
+  int val;
+  TreeLinkNode *left, *right, *next;
+  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if ( root == NULL ) return;
+        TreeLinkNode *pre = root;
+        TreeLinkNode *cur = NULL;
+        while( pre->left ){
+            cur = pre;
+            while( cur ){
+                cur->left->next = cur->right;
+                if ( cur->next ) cur->right->next = cur->next->left;
+                cur = cur->next;
+            }
+            pre = pre->left;
+        }
+        
+    }
+};
