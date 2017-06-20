5393c7aae777312b63d3e3982cbd00a24b065677
xincheng <wxcxincheng@163.com>
2017-06-20 23:02:05 +0800

    gas_station

---
+++ Algorithm/gas_station/gas_station.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        if( gas.empty() || cost.empty() || gas.size() != cost.size() ) return -1;
+        int start = gas.size()-1;
+        int end = 0;
+        int sum = gas[start] - cost[start];
+        while( start > end ){
+            if( sum >=0 ){
+                sum += gas[end] - cost[end];
+                ++end;
+            }
+            else{
+                --start;
+                sum += gas[start] - cost[start];
+            }
+        }
+        
+        return  sum>=0? start:-1;
+    }
+};
+
+
+
+int main()
+{
+	int a[] = {1,1,4,8};
+	vector<int> gas(a,a+4);
+	int b[] = {1,11,1,1};
+	vector<int> cost(b,b+4);
+	Solution s;
+	cout<<s.canCompleteCircuit(gas,cost)<<endl;
+}
